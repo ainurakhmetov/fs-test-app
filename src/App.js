e19dd6fcfb3eb0f2f14d1c526202039962ada097
Ainur Akhmetov <akhmetovainur@gmail.com>
2018-11-07 14:15:02 +0300

    styles fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.module.css';
-import Header from './organisms/Header';
 import Homepage from './pages/Homepage';
-import Footer from './organisms/Footer';
-
 class App extends Component {
 
 
