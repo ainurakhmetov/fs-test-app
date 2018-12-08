96fbb377342a1ee7b0b3423c62d01d81e06b70ea
Ainur Akhmetov <akhmetovainur@gmail.com>
2018-12-08 17:19:10 +0300

    Updates

---
+++ precache-manifest.837c527d55f92d7eb0db7a5d6e83ae71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d05001b51d6cfcfa656",
+    "url": "/fs_app/static/js/runtime~main.3d05001b.js"
+  },
+  {
+    "revision": "dc7c5778465ca05281aa",
+    "url": "/fs_app/static/js/main.dc7c5778.chunk.js"
+  },
+  {
+    "revision": "702c8a5a7d637906b79f",
+    "url": "/fs_app/static/js/1.702c8a5a.chunk.js"
+  },
+  {
+    "revision": "dc7c5778465ca05281aa",
+    "url": "/fs_app/static/css/main.44342f62.chunk.css"
+  },
+  {
+    "revision": "1595cffa205279f22fd842caefc2f884",
+    "url": "/fs_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fs_app/precache-manifest.7b8cfa5c5b23bcf96f9afa4379650981.js"
+  "/fs_app/precache-manifest.837c527d55f92d7eb0db7a5d6e83ae71.js"
 );
 
 workbox.clientsClaim();
